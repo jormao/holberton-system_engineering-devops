0842e3133134d4882b9a16ef1c774906837b86f9
jormao <jormao@gmail.com>
2020-03-24 09:21:08 -0500

    fix task 2 with env

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 exec {'killmenow':
     command =>  'pkill -f killmenow',
-    path    =>  'usr/bin',
+    path    =>  ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
+    provider => 'shell',
 }
