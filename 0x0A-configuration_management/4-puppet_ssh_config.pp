3edc72f55e60dfb104d2577a9404e54886b1ff26
jormao <jormao@gmail.com>
2020-03-24 13:39:29 -0500

    task 4

--- 0x0A-configuration_management/4-puppet_ssh_config.pp
+++ 0x0A-configuration_management/4-puppet_ssh_config.pp
@@ -3,10 +3,14 @@ file { '/etc/ssh/ssh_config':
   ensure  =>  present,
 }
 file_line { 'private_key':
+  ensure  =>  present,
   path  =>  '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/holberton',
+  match =>  '^IdentityFile.*$'
 }
 file_line { 'Pass_authent':
+  ensure  =>  present,
   path  =>  '/etc/ssh/ssh_config',
   line  =>  'PasswordAuthentication no',
+  match =>  '^PasswordAuthentication.*$'
 }
