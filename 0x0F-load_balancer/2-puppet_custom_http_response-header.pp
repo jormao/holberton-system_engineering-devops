15784111222438516c7363ec9e85f45d5656e402
jormao <jormao@gmail.com>
2020-04-03 21:23:00 -0500

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,6 @@
 # automate the task of creating a custom 
 # HTTP header response, but with Puppet.
 exec {'configure HTTP':
-  command => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '43i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'' /etc/nginx/sites-available/default && sed -i '12i add_header X-Served-By $hostname;'' /etc/nginx/nginx.conf && service nginx start",
+  command => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '43i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sed -i '12i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
   provider => 'shell',
 }
