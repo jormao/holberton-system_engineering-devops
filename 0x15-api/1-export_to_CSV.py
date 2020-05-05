cc9122bb46276778d13fbda5a69e366334fae890
jormao <jormao@gmail.com>
2020-05-04 21:27:42 -0500

    task 1 fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Using what you did in the task #0, extend your Python script to export data in the CSV format.
+""" Using what you did in the task #0, extend your Python script to export
+data in the CSV format.
 Requirements:
     Records all tasks that are owned by this employee
     Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
