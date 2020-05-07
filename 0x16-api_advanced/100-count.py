9c9571f76f01f143eaeeb30c4622bef9f15268d0
jormao <jormao@gmail.com>
2020-05-07 18:10:50 -0500

    task 3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" ecursive function that queries the Reddit API, parses the title
+of all hot articles, and prints a sorted count of given keywords
+"""
+from requests import get
+
+
+def count_words(subreddit, word_list, after="", counter={}, first=0):
+    """parses the title of all hot articles, and prints a sorted list."""
+    url = 'https://www.reddit.com'
+    path = '/r/{}/hot.json?after='.format(subreddit)
+    header = {'user-agent': 'jormao'}
+    r = get(url + path + str(after), headers=header, allow_redirects=False)
+    if (first == 0):
+        for word in word_list:
+            counter[word] = 0
+        first = 1
+    if (r.status_code in [302, 404]):
+        return(None)
+    else:
+        hot_post = r.json()['data']['children']
+        for hot in hot_post:
+            new_list = (hot['data']['title']).lower().split(" ")
+            for word in word_list:
+                for w_list in new_list:
+                    if word == w_list:
+                        counter[word] += 1
+        after = r.json()['data']['after']
+        if (after is not None):
+            count_words(subreddit, word_list, after, counter, first)
+        else:
+            p = sorted(counter.items())
+            for k, v in p:
+                if v != 0:
+                    print('{}: {}'.format(k, v))
