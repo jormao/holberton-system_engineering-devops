90a1ce0831363a8349af77fdf6c50f7f1ad82386
Jormao <jormao@gmail.com>
2020-05-20 15:31:07 +0000

    add restart server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,3 +2,6 @@
 exec { 'fix php error':
     command => '/bin/sed -i \'s/.phpp/.php\' /var/www/html/wp-settings.php'
 }
+exec { 'restart apache':
+    command => '/usr/sbin/service apache2 restart'
+}
