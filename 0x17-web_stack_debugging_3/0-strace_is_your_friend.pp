974058e84e9190351989a6ad4aff2d13d9c064ba
jormao <jormao@gmail.com>
2020-05-20 11:00:19 -0500

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #fix error
 exec { 'fix php error':
-    command => '/bin/sed -i \'s/.phpp/.php\' /var/www/html/wp-settings.php'
+    command => '/bin/sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php'
 }
 exec { 'restart apache':
     command => '/usr/sbin/service apache2 restart'
